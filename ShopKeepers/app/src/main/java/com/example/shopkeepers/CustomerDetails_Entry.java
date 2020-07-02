9b944fd79ead7e0f2384b9f6538b94b4bcf5571f
Bicon <minhazu.alam142@northsouth.edu>
2020-07-02 18:17:40 +0600

    Customer_list and Customer details entry (UI)

---
+++ ShopKeepers/app/src/main/java/com/example/shopkeepers/CustomerDetails_Entry.java
@@ -0,0 +1,14 @@
+package com.example.shopkeepers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerDetails_Entry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_details__entry);
+    }
+}
---
+++ ShopKeepers/app/src/main/java/com/example/shopkeepers/Customer_List_Activity.java
@@ -0,0 +1,14 @@
+package com.example.shopkeepers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Customer_List_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer__list_);
+    }
+}
