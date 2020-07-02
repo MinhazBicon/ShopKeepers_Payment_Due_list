05a132a91541e5ba60a8dd2013e1e2006258aae7
Bicon <minhazu.alam142@northsouth.edu>
2020-07-02 10:52:19 +0600

    SignUp_page (PopUp window)

--- ShopKeepers/app/src/main/java/com/example/shopkeepers/LoginActivity.java
+++ ShopKeepers/app/src/main/java/com/example/shopkeepers/LoginActivity.java
@@ -2,15 +2,19 @@ package com.example.shopkeepers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private Typeface font;
     private TextView shopkeeperLogo;
+    private Button SignUp, Login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +27,20 @@ public class LoginActivity extends AppCompatActivity {
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         shopkeeperLogo = findViewById(R.id.ShopKeeperLogo);
-
+        SignUp = findViewById(R.id.Login_signUp_btn);
+        Login =  findViewById(R.id.Login_btn);
         font = Typeface.createFromAsset(getAssets(),"PermanentMarker_Regular.ttf");
         shopkeeperLogo.setTypeface(font);
+
+        SignUp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == SignUp){
+            Intent intent = new Intent(LoginActivity.this, SignUp_PopUp_Window.class);
+            startActivity(intent);
+
+        }
     }
 }
--- ShopKeepers/app/src/main/java/com/example/shopkeepers/SignUp_PopUp_Window.java
+++ ShopKeepers/app/src/main/java/com/example/shopkeepers/SignUp_PopUp_Window.java
@@ -1,6 +1,50 @@
 package com.example.shopkeepers;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
-public class SignUp_PopUp_Window extends Activity {
+import androidx.annotation.Nullable;
+
+public class SignUp_PopUp_Window extends Activity implements View.OnClickListener {
+    //int flag = #ACFAEF;
+    private Button cancel, done;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_popup);
+
+        cancel = findViewById(R.id.signUp_cancel_btn);
+        done = findViewById(R.id.signUp_done_btn);
+        cancel.setOnClickListener(this);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        int width = displayMetrics.widthPixels;
+        int height = displayMetrics.heightPixels;
+       // getWindow().setBackgroundDrawableResource(flag);
+        getWindow().setLayout((int) (width*.9),(int) (height*.95));
+
+        //setting the Popup window position
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y = -75;
+        getWindow().setAttributes(params);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == cancel){
+            finish();
+        }
+        if (view == done){
+            finish();
+        }
+    }
 }
